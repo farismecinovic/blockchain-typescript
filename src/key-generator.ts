9af9bc1adf7bd9b3fe6270bab515740e96c9246a
Faris <faris.mecinovic.np@gmail.com>
2023-01-03 20:51:47 +0100

    removed unused imports/logs

--- src/key-generator.ts
+++ src/key-generator.ts
@@ -6,11 +6,3 @@ const key = ec.genKeyPair();
 
 export const publicKey = key.getPublic('hex');
 export const privateKey = key.getPrivate('hex');
-
-console.log();
-
-console.log('Private key:', privateKey);
-
-console.log();
-
-console.log('Public key:', publicKey);
